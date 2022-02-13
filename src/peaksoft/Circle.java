793535d21f66e4e7bc734fdd15664802abf16e16
daudali94 <dnishanbai@gmail.com>
2022-02-13 14:21:14 +0600

    16 commit

---
+++ src/peaksoft/Circle.java
@@ -0,0 +1,23 @@
+package peaksoft;
+public class Circle {
+    public static final double pi = 3.141592;
+    private  double radius;
+    public Circle() {}
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public void area (){
+        double i = pi*(radius*radius);
+        System.out.println("Плошадь area "+i);
+    }
+    public void circumference(){
+        double i = (pi*2*radius);
+        System.out.println("Длина radius "+i);
+    }
+}
---
+++ src/peaksoft/Main.java
@@ -0,0 +1,11 @@
+package peaksoft;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Circle circle = new Circle();
+    circle.setRadius(5);
+    circle.area();
+    circle.circumference();
+    }
+}
